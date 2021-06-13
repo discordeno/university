8f24404716d232ea62b6dacfe2171a4d575581bd
Lumap <lumapbk@gmail.com>
2021-06-13 19:29:17 +0200

    Update DDButtonInteraction.ts

--- src/Structures/DDButtonInteraction.ts
+++ src/Structures/DDButtonInteraction.ts
@@ -3,13 +3,14 @@ import {
   Interaction,
   ButtonData
 } from "../../deps.ts"
+import Client from "../Client.ts"
 
 export class DDButtonInteraction extends BaseInteraction {
   /** Interaction data */
   data: ButtonData
   
-  constructor(payload: Interaction) {
-    super(interaction)
+  constructor(client: Client, payload: Interaction) {
+    super(client, interaction)
     this.data=this.payload.data
   }
 }
