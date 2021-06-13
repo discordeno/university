e349f4ceb75bcefc14f3d2e0676fd4e1dd2db682
Lumap <lumapbk@gmail.com>
2021-06-13 19:29:51 +0200

    Update DDSlashInteraction.ts

--- src/Structures/DDSlashInteraction.ts
+++ src/Structures/DDSlashInteraction.ts
@@ -3,13 +3,14 @@ import {
   Interaction,
   ApplicationCommandInteractionData
 } from "../../deps.ts"
+import Client from "../Client.ts"
 
 export class DDSlashInteraction extends BaseInteraction {
   /** Interaction data */
   data: ApplicationCommandInteractionData
   
-  constructor(payload: Interaction) {
-    super(interaction)
+  constructor(client: Client,payload: Interaction) {
+    super(client,payload)
     this.data=this.payload.data
   }
 }
