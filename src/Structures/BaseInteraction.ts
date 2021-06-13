1f57993a050e25e274807ea5c8c1acb17ea3debf
Lumap <lumapbk@gmail.com>
2021-06-13 19:30:18 +0200

    Update BaseInteraction.ts

--- src/Structures/BaseInteraction.ts
+++ src/Structures/BaseInteraction.ts
@@ -28,8 +28,6 @@ export class DDBaseInteraction {
   guildId?: string;
   /** The channel it was sent from */
   channelId?: string;
-  /** DO NOT USE THIS - Used to get data with the right type once */
-  #payload: Interaction;
   /** The member object */
   member?: GuildMemberWithUser
   /** User object for the invoking user, if invoked in a DM */
@@ -46,7 +44,6 @@ export class DDBaseInteraction {
     this.applicationId=payload.applicationId
     this.type=payload.type
     this.token=payload.token
-    this.#payload=payload
     if (payload.member) {this.member= payload.member as GuildMemberWithUser}
     if (payload.guildId) {this.guildId=payload.guildId}
     if (payload.user) {this.user=payload.user as User}
